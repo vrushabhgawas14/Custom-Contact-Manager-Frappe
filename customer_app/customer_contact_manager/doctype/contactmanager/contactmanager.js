84034f4ce6bd030ae486c77f6fd3ef4fed4decce
vrushabhgawas14 <vrushabhgawas14@gmail.com>
2024-11-19 09:48:09 +0000

    First Commit

---
+++ customer_app/customer_contact_manager/doctype/contactmanager/contactmanager.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, vrushabh and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("ContactManager", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ customer_app/customer_contact_manager/doctype/contactmanager/test_contactmanager.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, vrushabh and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestContactManager(FrappeTestCase):
+	pass
